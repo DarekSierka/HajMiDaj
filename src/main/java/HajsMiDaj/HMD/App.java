79e821de64843ce1719b951f0747af14293b0579
DarekSierka <darek.sierka9@gamil.com>
2016-04-13 19:30:37 +0200

    Testy2

--- src/main/java/HajsMiDaj/HMD/App.java
+++ src/main/java/HajsMiDaj/HMD/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Takie tam teściki!" );
+        System.out.println( "Takie tam teściki2!" );
     }
 }
