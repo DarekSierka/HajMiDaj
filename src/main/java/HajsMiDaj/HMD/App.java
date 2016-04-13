101c07b0d7f487b3b549d3076ac73b741631d1c6
DarekSierka <darek.sierka9@gamil.com>
2016-04-13 19:14:04 +0200

    Let's start

---
+++ src/main/java/HajsMiDaj/HMD/App.java
@@ -0,0 +1,13 @@
+package HajsMiDaj.HMD;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
