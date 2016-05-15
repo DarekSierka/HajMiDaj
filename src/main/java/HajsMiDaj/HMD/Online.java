979219143dfe19dc014acee4b171b74b04a92f2a
undertaker2 <undertaker2-94@o2.pl>
2016-05-15 22:52:43 +0200

    navbar

--- src/main/java/HajsMiDaj/HMD/Online.java
+++ src/main/java/HajsMiDaj/HMD/Online.java
@@ -19,6 +19,9 @@ public class Online {
 	private JLabel logo;
 	
 	private JButton wyloguj;
+	private JButton navbar1;
+	private JButton navbar2;
+	private JButton navbar3;
 	
 	private int oknoX = 1024;
 	private int oknoY = 600;
@@ -32,6 +35,10 @@ public class Online {
 		logo= new JLabel(new ImageIcon("pictures/Fox-rounded-small.png"));
 		
 		wyloguj = new JButton("Wyloguj");
+		navbar1 = new JButton("Moje zakłady");
+		navbar2 = new JButton("Postaw zakład");
+		navbar3 = new JButton("Tabela rozgrywek");
+		
 		
 		if(setParams()){
 			System.out.println("Ustaweinie parametrów zakończone powodzeniem!");
@@ -59,15 +66,22 @@ public class Online {
 	}
 	
 	private boolean setElements(){
-		int buttonsWidth = 100, buttonsHeight = 30;
-		
+		int buttonsWidth = 150, buttonsHeight = 30;
+		int navbarX=30, navbarY=120;
 		logo.setBounds(10, 10, 180, 100);
 		logo.setVisible(true);
 		
 		wyloguj.setBounds(oknoX - buttonsWidth - 50, 10, buttonsWidth,buttonsHeight);
 		wyloguj.setVisible(true);
 		
+		navbar1.setBounds(navbarX, navbarY, buttonsWidth, buttonsHeight);
+		navbar2.setBounds(navbarX,navbarY+buttonsHeight,buttonsWidth,buttonsHeight);
+		navbar3.setBounds(navbarX, navbarY+ 2*buttonsHeight, buttonsWidth, buttonsHeight);
+		
 		obraz.add(wyloguj);
+		obraz.add(navbar1);
+		obraz.add(navbar2);
+		obraz.add(navbar3);
 		obraz.add(logo);
 		
 		return true;
@@ -80,18 +94,37 @@ public class Online {
 		okno.dispose();
 	}
 	
+	private void mojeZakladyClicked(){
+		
+	}
+	
+	private void postawZakladClicked(){
+		
+	}
+	
+	private void tabelaRozgrywekClicked(){
+		
+	}
+	
 	private boolean setListenners(){
-		wyloguj.addActionListener(new Listener());
+		wyloguj.addActionListener(new Listenner());
+		navbar1.addActionListener(new Listenner());
+		navbar2.addActionListener(new Listenner());
+		navbar3.addActionListener(new Listenner());
 		return true;
 	}
 		
-	class Listener implements ActionListener{
+	class Listenner implements ActionListener{
 		public void actionPerformed(ActionEvent e) {
 	      	
-	      	if(e.getActionCommand().equals("Wyloguj")){
+	      	if(e.getActionCommand().equals("Wyloguj"))
 	      		wylogujClicked();
-	      	}
-	      	      	
+	      	else if(e.getActionCommand().equals("Moje zakłady"))
+	      		mojeZakladyClicked();
+	      	else if(e.getActionCommand().equals("Postaw zaklad"))
+	      		postawZakladClicked();
+	      	else if(e.getActionCommand().equals("Tabela rozgrywek"))
+	      		tabelaRozgrywekClicked();
 		} 
 	}	
 	
