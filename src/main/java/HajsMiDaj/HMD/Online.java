d4858a3a37f74bfacce3383b55c49ab9c71e8569
undertaker2 <undertaker2-94@o2.pl>
2016-05-16 00:42:53 +0200

    Wyswietlanie zakladow, tabeli i obstawianie meczu (prototypy)

--- src/main/java/HajsMiDaj/HMD/Online.java
+++ src/main/java/HajsMiDaj/HMD/Online.java
@@ -7,6 +7,8 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTable;
+import javax.swing.JComboBox;
+import javax.swing.JTextField;
 
 import HajsMiDaj.HMD.Register.Listener;
 
@@ -20,6 +22,13 @@ public class Online {
 	private JLabel logo;
 	
 	private JTable tabelaRozgrywek;
+	private JTable zakladyUzytkownika;
+
+	private JComboBox mecz;
+	private JComboBox typ;
+	private JLabel kurs;
+	private JTextField stawka;
+	private JButton zatwierdz;
 	
 	private JButton wyloguj;
 	private JButton navbar1;
@@ -81,6 +90,28 @@ public class Online {
 		navbar2.setBounds(navbarX,navbarY+buttonsHeight,buttonsWidth,buttonsHeight);
 		navbar3.setBounds(navbarX, navbarY+ 2*buttonsHeight, buttonsWidth, buttonsHeight);
 		
+		tabelaRozgrywek = new JTable();
+		tabelaRozgrywek.setVisible(false);
+		obraz.add(tabelaRozgrywek);
+		
+		zakladyUzytkownika = new JTable();
+		zakladyUzytkownika.setVisible(false);
+		obraz.add(zakladyUzytkownika);
+		
+		kurs = new JLabel();
+		kurs.setVisible(false);
+		kurs.setBounds(255, 355, 150, 30);
+		obraz.add(kurs);
+		
+		stawka = new JTextField();
+		stawka.setBounds(255, 415, 150, 30);
+		stawka.setVisible(false);
+		obraz.add(typ);
+		
+		zatwierdz = new JButton();
+		zatwierdz.setBounds(255,475,450,30);
+		zatwierdz.setVisible(false);
+		obraz.add(zatwierdz);
 		
 		obraz.add(wyloguj);
 		obraz.add(navbar1);
@@ -90,9 +121,80 @@ public class Online {
 		
 		return true;
 	}
+		
 	
-	private void setTabelaRozgrywek(){
+	private void wyczyscContent(){
+		zakladyUzytkownika.setVisible(false);
+		tabelaRozgrywek.setVisible(false);
+		mecz.setVisible(false);
+		typ.setVisible(false);
+		kurs.setVisible(false);
+		stawka.setVisible(false);
+		zatwierdz.setVisible(false);
+	}
 	
+	private void wylogujClicked(){
+		
+		JOptionPane.showMessageDialog(null,"Wylogowano!");
+		new Okno();
+		okno.dispose();
+	}
+	
+	private void mojeZakladyClicked(){
+		wyczyscContent();
+		String[] headers = {
+				"Gospodarze",
+				"Goscie",
+				"Typ",
+				"Stawka",
+				"Kurs"
+		};
+		//do tablicy data trzeba pobrać dane z bazy danych
+		Object[][] data = {
+				{"GKS Tarnovia","Warta Międzychód", "1","14.00","1.72"},
+				{"Sparta Oborniki","Iskra Szydłowo", "1X","2.00", "1.81"},
+				{"Lubuszanin Trzcianka","GKS Dopiewo", "X2","4.50","2.16"},
+				{"Mieszko Gniezno","Pelikan Niechanowo", "2","35.00", "1.32"}
+		};
+		zakladyUzytkownika = new JTable(data,headers);
+		zakladyUzytkownika.setBounds(250,120, 700, 400);
+		zakladyUzytkownika.setVisible(true);
+	}
+	
+	private void postawZakladClicked(){
+		wyczyscContent();
+		//do tablicy data trzeba pobrać dane z bazy danych
+		String[] data = {
+				"GKS Tarnovia - Warta Międzychód",
+				 "Sparta Oborniki - Iskra Szydłowo",
+				 "Lubuszanin Trzcianka - GKS Dopiewo",
+				 "Mieszko Gniezno - Pelikan Niechanowo"
+		}; 
+		mecz = new JComboBox<String>(data);
+		mecz.setBounds(255,255,150,30);
+		mecz.setVisible(true);
+		
+		String[] data2 = {
+				"1",
+				"1X",
+				"X",
+				"X2",
+				"2"
+		};
+		typ = new JComboBox<String>(data2);
+		typ.setBounds(255,295,150,30);
+		typ.setVisible(false);
+		
+		kurs.setText("");
+		kurs.setVisible(true);
+		stawka.setVisible(true);
+		zatwierdz.setVisible(true);
+		obraz.add(mecz);
+		obraz.add(typ);
+	}
+	
+	private void tabelaRozgrywekClicked(){
+		wyczyscContent();
 		String[] headers = {
 				"LP",
 				"Nazwa",
@@ -102,6 +204,7 @@ public class Online {
 				"Porazki",
 				"Bilans bramek"
 		};
+		//do tablicy data trzeba dodać dane z bazy danych
 		Object[][] data = {
 				{"1","GKS Tarnovia", "56","17","5","2","+31"},
 				{"2","Warta Międzychód", "49","14","7","3","+19"},
@@ -113,28 +216,13 @@ public class Online {
 		tabelaRozgrywek = new JTable(data,headers);
 		tabelaRozgrywek.setBounds(250,120, 700, 400);
 		tabelaRozgrywek.setVisible(true);
-		
-		obraz.add(tabelaRozgrywek);
-	
 	}
 	
-	private void wylogujClicked(){
-		
-		JOptionPane.showMessageDialog(null,"Wylogowano!");
-		new Okno();
-		okno.dispose();
-	}
-	
-	private void mojeZakladyClicked(){
-		
-	}
-	
-	private void postawZakladClicked(){
-		
-	}
-	
-	private void tabelaRozgrywekClicked(){
-		
+	private void zatwierdzClicked(){
+		//sprawdzenie hajsów
+		//sprawdzanie czy nie obstawił już tego meczu
+		//dodanie zakładu
+		JOptionPane.showMessageDialog(null,"Dodano nowy zakład!");
 	}
 	
 	private boolean setListenners(){
@@ -142,6 +230,7 @@ public class Online {
 		navbar1.addActionListener(new Listenner());
 		navbar2.addActionListener(new Listenner());
 		navbar3.addActionListener(new Listenner());
+		zatwierdz.addActionListener(new Listenner());
 		return true;
 	}
 		
@@ -156,6 +245,8 @@ public class Online {
 	      		postawZakladClicked();
 	      	else if(e.getActionCommand().equals("Tabela rozgrywek"))
 	      		tabelaRozgrywekClicked();
+	      	else if(e.getActionCommand().equals("Zatwierdz"))
+	      		zatwierdzClicked();
 		} 
 	}	
 	
